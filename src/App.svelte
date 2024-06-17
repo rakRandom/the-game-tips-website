7043bb26408c397e5147b7c6a525602d2e6c4a47
Fellipe Leonardo <112525075+rakRandom@users.noreply.github.com>
2024-06-17 17:36:37 -0300

    feat: routing system improved

--- src/App.svelte
+++ src/App.svelte
@@ -4,11 +4,26 @@
     import Index from './routes/Index.svelte';
     import Help from './routes/Help.svelte';
     import NotFound from './routes/NotFound.svelte';
+    import Admin from './routes/Admin.svelte';
+    import About from './routes/About.svelte';
+    import Category from './routes/Category.svelte';
+    import Contact from './routes/Contact.svelte';
+    import Article from './routes/Article.svelte';
+    import CreateAccount from './routes/CreateAccount.svelte';
+    import UserPage from './routes/UserPage.svelte';
+    import Login from './routes/Login.svelte';
 
     let routes = {
         '/': Index,
         '/help': Help,
-
+        '/about': About,
+        '/contact': Contact,
+        '/login': Login,
+        '/create-account': CreateAccount,
+        '/user/:id': UserPage,
+        '/article/:id': Article,
+        '/category/:id': Category,
+        '/admin': Admin,
         '*': NotFound
     }
 </script>
