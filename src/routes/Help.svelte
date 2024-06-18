e67cc5bd8a4f5edc9d539fa3e3411783d9de4434
Fellipe Leonardo <112525075+rakRandom@users.noreply.github.com>
2024-06-18 15:40:06 +0000

    feat: Help page

--- src/routes/Help.svelte
+++ src/routes/Help.svelte
@@ -1,9 +1,38 @@
 <script>
+    import Footer from "../components/Footer.svelte";
+    import Header from "../components/Header.svelte";
+    import Section from "../components/help/Section.svelte";
 
+    let values = [
+        {legend: "LEGENDA", contents: [["B", "A"], ["B", "A"], ["B", "A"], ["B", "A"]]}, 
+        {legend: "LEGENDA", contents: [["B", "A"], ["B", "A"], ["B", "A"], ["B", "A"]]}, 
+        {legend: "LEGENDA", contents: [["B", "A"], ["B", "A"], ["B", "A"], ["B", "A"]]}, 
+        {legend: "LEGENDA", contents: [["B", "A"], ["B", "A"], ["B", "A"], ["B", "A"]]}, 
+    ];
 </script>
 
-<div>
+<div class="flex flex-col w-full min-h-screen bg-[#F6F6F6] dark:bg-[#000B10] text-[#121212] dark:text-white">
+    <Header className="sticky top-0" />
+    <main class="flex flex-col gap-32 flex-1 max-w-[1024px] w-full mx-auto py-24">
+        <div>
+            <h1 class="text-5xl text-[#1E9B8F] dark:text-[#69D3C9] font-title-semibold mb-16">
+                Help Center
+            </h1>
     
+            <p class="text-[1.5rem] px-4 indent-8">
+                If you need assistance with our services or tools, you’ve come to the right place. We strive to make your experience even better, so we make it easy for you to get help and support.
+            </p>
+    
+            <p class="text-[1.5rem] px-4 mt-4 indent-8">
+                Here you can get help in any way you like by reading how our tools work or talking to our support team.
+            </p>
+        </div>
+        
+        {#each values as value}
+        <Section {...value} />
+        {/each}
+    </main>
+    <Footer />
 </div>
 
 <style>
