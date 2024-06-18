b6619e3cee3d8c63d75ad8809a646c9e8df87ea9
Matheus Santana <130666046+mathsant-js@users.noreply.github.com>
2024-06-18 04:11:21 +0000

    style: added AboutDevTemplate to the About page

--- src/components/about_page/AboutDevTemplate.svelte
+++ src/components/about_page/AboutDevTemplate.svelte
@@ -1,5 +1,5 @@
 <script>
-    export let name = "Dev Name", image = "src\assets\img\profile picture placeholder.png", profileLink, linkedin = "https://br.linkedin.com/", github = "https://github.com/", whatsapp = "", email = "lucastino2007@gmail.com.br",
+    export let name = "Dev Name", image = "src\assets\img\profile picture placeholder.png", profileLink = "", linkedin = "https://br.linkedin.com/", github = "https://github.com/", whatsapp = "", email = "lucastino2007@gmail.com.br",
         description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing. ";
 </script>
 
--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -1,14 +1,40 @@
 <script>
   import Header from ".././components/Header.svelte";
   import MainAbout from "../components/about_page/MainAbout.svelte";
-  import SimpleSection from ".././components/SimpleSection.svelte";
+  import AboutDevTemplate from "../components/about_page/AboutDevTemplate.svelte";
   import Footer from ".././components/Footer.svelte";
 
   let src = "../public/backgroundImageAbout.svg";
+
+  let valores = [
+    { name: "Fellipe Leonardo Peixoto Cunha", description: "Mó paz" },
+    { name: "Matheus Jorge Santana", description: "" },
+    { name: "Bárbara Fernandes Rampazi", description: "" },
+    { name: "Henrique", description: "" },
+    { name: "Lucas Tino Rosa", description: "" },
+  ];
 </script>
 
 <div class="w-full min-h-screen bg-[#001219]">
   <Header className="sticky top-0" />
   <MainAbout />
+  <div class="flex items-center justify-center m-5">
+    <h1
+      class="text-[#69D3C9] text-[36px] font-title-semibold block text-center"
+    >
+      Our Team
+    </h1>
+  </div>
+  <div>
+    {#each valores as valor}
+      <AboutDevTemplate {...valor} />
+    {/each}
+  </div>
+  <div class="flex items-center justify-center m-10">
+    <h2 class="text-white text-[36px] font-title-bold block text-center">
+      Success is not definitive, failure is not fatal: It is the courage to
+      continue that counts.
+    </h2>
+  </div>
   <Footer />
 </div>
