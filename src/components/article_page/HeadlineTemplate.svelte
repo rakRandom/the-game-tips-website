5a6b02bf08b8fd116c8b9d948515f9ec3c74b5d0
Matheus Santana <130666046+mathsant-js@users.noreply.github.com>
2024-06-18 22:44:30 +0000

    fix: improving Headline Template

--- src/components/article_page/HeadlineTemplate.svelte
+++ src/components/article_page/HeadlineTemplate.svelte
@@ -1,17 +1,29 @@
 <script>
   let eldenRingImg = "../src/assets/img/eldenRingImage.svg";
+  let userImage = "../public/userImage.svg";
 </script>
 
 <div class="flex flex-col w-full min-h-screen">
-    <main class="flex flex-col gap-32 flex-1 max-w-[1024px] w-full mx-auto py-24">
-  <div>
-    <h1
-      class="flex items-start text-5xl text-[#1E9B8F] dark:text-[#69D3C9] font-title-semibold mb-12"
-    >
-      ELDEN RING
-    </h1>
-    <h3 class="text-3xl font-title-regular mb-10">Where to Find Lucerne in Elden Ring</h3>
-    <img src={eldenRingImg} alt="Elden Ring">
-  </div>
-</main>
+  <main class="flex flex-col gap-32 flex-1 max-w-[1024px] w-full mx-auto py-24">
+    <div>
+      <h1
+        class="flex items-start text-5xl text-[#1E9B8F] dark:text-[#69D3C9] font-title-semibold mb-12"
+      >
+        ELDEN RING
+      </h1>
+      <h3 class="text-3xl font-title-regular mb-10">
+        Where to Find Lucerne in Elden Ring
+      </h3>
+      <img src={eldenRingImg} alt="Elden Ring" class="mb-8" />
+      <div class="flex flex-row items-center">
+        <img src={userImage} alt="User" />
+        <p class="text-[#1E9B8F] dark:text-[#69D3C9] mx-5">
+          By Valteci Junior T. • Mai 28, 2024
+        </p>
+      </div>
+      <p class="text-[#1E9B8F] dark:text-[#69D3C9] flex justify-center mt-2">
+        _______________________________________________________________________________________________________________________________________________________
+      </p>
+    </div>
+  </main>
 </div>
