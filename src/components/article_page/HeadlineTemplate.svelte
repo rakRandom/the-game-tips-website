8239332e91edbfa4b0e3c43360c364e9c067fd6b
Matheus Santana <130666046+mathsant-js@users.noreply.github.com>
2024-06-18 22:13:16 +0000

    style: Heasline Template of article page was added

---
+++ src/components/article_page/HeadlineTemplate.svelte
@@ -0,0 +1,17 @@
+<script>
+  let eldenRingImg = "../src/assets/img/eldenRingImage.svg";
+</script>
+
+<div class="flex flex-col w-full min-h-screen">
+    <main class="flex flex-col gap-32 flex-1 max-w-[1024px] w-full mx-auto py-24">
+  <div>
+    <h1
+      class="flex items-start text-5xl text-[#1E9B8F] dark:text-[#69D3C9] font-title-semibold mb-12"
+    >
+      ELDEN RING
+    </h1>
+    <h3 class="text-3xl font-title-regular mb-10">Where to Find Lucerne in Elden Ring</h3>
+    <img src={eldenRingImg} alt="Elden Ring">
+  </div>
+</main>
+</div>
---
+++ src/routes/Article.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Header from "../components/Header.svelte";
+    import HeadlineTemplate from "../components/article_page/HeadlineTemplate.svelte";
+    import Footer from "../components/Footer.svelte";
+</script>
+
+<div class="w-full min-h-screen bg-[#F6F6F6] dark:bg-[#000B10] text-[#121212] dark:text-white">
+    <Header className="sticky top-0" />
+    <HeadlineTemplate />
+    <Footer />
+</div>
