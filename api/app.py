7123fc07288676a193985913c5c066c8141b1cde
Fellipe Leonardo <112525075+rakRandom@users.noreply.github.com>
2024-06-19 11:19:24 +0000

    fix: delete api

--- api/app.py
+++
@@ -1,6 +0,0 @@
-from api            import app
-# from database       import *
-
-
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port="5050", debug=True)
--- api/config/libs.py
+++
@@ -1,3 +0,0 @@
-from flask import Flask, request, redirect, jsonify
-from flask_cors import CORS, cross_origin
-from flask_sqlalchemy import SQLAlchemy
