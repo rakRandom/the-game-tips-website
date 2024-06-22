ca5f71d201987eae72bc0bf95c6902508881798d
Fellipe Leonardo <112525075+rakRandom@users.noreply.github.com>
2024-06-22 07:29:54 +0000

    refactor: CategoryBar full responsivity

--- src/components/landing_page/CategoryBar.svelte
+++ src/components/landing_page/CategoryBar.svelte
@@ -9,11 +9,11 @@
     export let imgSrc;
 </script>
 
-<div class="w-full h-[567px] bg-[#00141E]">
-    <div class="max-w-[1024px] h-full mx-auto pt-[52px] pb-[100px]">
-        <div class="flex gap-[30px] h-full w-full">
-            <div class="flex flex-col gap-4 w-[300px]">
-                <h2 class="uppercase text-[38px] text-[#2EC4B6] break-words">
+<div class="w-full lg:max-h-[567px] bg-[#00141E]">
+    <div class="max-w-[1024px] h-full mx-auto pt-[32px] md:pt-0 lg:pb-[32px] lg:pt-[32px] lg:px-[32px]">
+        <div class="flex flex-col md:flex-row gap-[30px] h-full w-full">
+            <div class="flex flex-col gap-4 w-fit md:w-[300px] md:max-h-[438px] lg:max-h-full px-[32px] md:pr-0 md:py-[32px] lg:px-0 lg:py-0">
+                <h2 class="uppercase text-[38px] text-[#2EC4B6] font-title-medium break-words">
                     {gameTitle}
                 </h2>
                 <p class="flex-1 text-lg text-ellipsis overflow-hidden">
@@ -22,7 +22,7 @@
                 <RedirectButton href={categoryPage} />
             </div>
             <div class="flex-1 flex items-center gap-10 h-full overflow-x-auto">
-                <img src={fetchImage(imgSrc)} alt="" class="bg-white object-cover w-full h-full rounded-lg">
+                <img src={fetchImage(imgSrc)} alt="" class="w-full bg-white object-cover aspect-[14/10] max-md:aspect-video max-lg:aspect-square lg:rounded-lg lg:h-full">
             </div>
         </div>
     </div>
