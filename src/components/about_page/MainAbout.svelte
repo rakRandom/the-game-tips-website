bab2de63c42cbab6da6fde52498dbd57bf1eddec
Fellipe Leonardo <112525075+rakRandom@users.noreply.github.com>
2024-06-25 11:13:01 +0000

    test: fix MainAbout background image

--- src/components/about_page/MainAbout.svelte
+++ src/components/about_page/MainAbout.svelte
@@ -3,17 +3,21 @@
 </script>
 
 <div class="w-full max-w-[1280px] h-fit mx-auto lg:bg-[#001B28] lg:bg-[url('backgroundImageAbout.svg')] lg:bg-cover xl:rounded-b-lg">
-    <div class="flex items-center justify-center w-full h-full lg:py-24">
-        <div class="flex flex-col lg:flex-row lg:items-center lg:gap-16 lg:w-[768px] h-fit lg:p-8 lg:bg-black lg:bg-opacity-50 lg:rounded-lg">
-            <div class="px-[16px] md:px-[32px] lg:px-0">
-                <h1 class="text-[#69D3C9] text-[2.5rem] lg:text-[2rem] font-title-semibold max-lg:text-center">About Us</h1>
-                <p class="text-[1.2rem] text-white text-justify mt-4">
-                    {aboutText}
-                </p>
-            </div>
+    <div class="relative h-full w-full">
+        <img src="backgroundImageAbout.svg" alt="" class="hidden">
     
-            <div class="flex justify-center w-full mt-8 p-2 py-8 bg-[#001B28] lg:bg-transparent">
-                <img src="logoImage.svg" alt="Logo" class="max-lg:w-[100px] lg:min-w-[150px]" />
+        <div class="flex items-center justify-center w-full h-full lg:py-24">
+            <div class="flex flex-col lg:flex-row lg:items-center lg:gap-16 lg:w-[768px] h-fit lg:p-8 lg:bg-black lg:bg-opacity-50 lg:rounded-lg">
+                <div class="px-[16px] md:px-[32px] lg:px-0">
+                    <h1 class="text-[#69D3C9] text-[2.5rem] lg:text-[2rem] font-title-semibold max-lg:text-center">About Us</h1>
+                    <p class="text-[1.2rem] text-white text-justify mt-4">
+                        {aboutText}
+                    </p>
+                </div>
+        
+                <div class="flex justify-center w-full mt-8 p-2 py-8 bg-[#001B28] lg:bg-transparent">
+                    <img src="logoImage.svg" alt="Logo" class="max-lg:w-[100px] lg:min-w-[150px]" />
+                </div>
             </div>
         </div>
     </div>
