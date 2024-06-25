369825e98aba1c070309231a40c2c1a391d1dd4a
Fellipe Leonardo <112525075+rakRandom@users.noreply.github.com>
2024-06-25 06:52:13 +0000

    feat: improve Bullet Tip Item

--- src/components/landing_page/BulletTipItem.svelte
+++ src/components/landing_page/BulletTipItem.svelte
@@ -9,9 +9,9 @@
     export let icon;
     export let image;
 
-    let foo = "hidden";
-    let show = () => {foo = "";};
-    let hide = () => {foo = "hidden";};
+    let visibility = "hidden";
+    let show = () => {visibility = "";};
+    let hide = () => {visibility = "hidden";};
 </script>
 
 <div class="flex flex-col items-center 
@@ -33,14 +33,14 @@
     </h1>
 </div>
 
-<div class="{foo} fixed z-50 top-0 left-0 w-screen h-screen bg-[#00000020] backdrop-blur-sm">
-    <div class="flex flex-col gap-4 items-center justify-center h-full w-full">
-        <div class="flex flex-col relative w-[380px] h-[560px] bg-[#051D26] rounded-xl overflow-hidden">
+<div class="{visibility} fixed z-50 top-0 left-0 w-svw h-svh bg-[#00000020] backdrop-blur-sm">
+    <div class="flex flex-col sm:gap-4 items-center justify-center h-full w-full">
+        <div class="flex flex-col relative w-full h-full sm:w-[380px] sm:h-[560px] bg-[#051D26] rounded-xl overflow-hidden">
             <button type="button" on:click={hide} 
                 class="absolute top-2 right-2 w-[30px] h-[30px] pb-[1px] bg-[#000000A0] rounded-full opacity-75 hover:opacity-100">
                 X
             </button>
-            <img src={fetchImage(image)} alt="" class="w-full h-[560px] object-cover">
+            <img src={fetchImage(image)} alt="" class="w-full h-full sm:h-[560px] object-cover">
             <div class="flex items-end h-[100px] w-full">
                 <div class="bg-[#001219] min-h-[100px] w-full px-4 py-2">
                     {phrase}
