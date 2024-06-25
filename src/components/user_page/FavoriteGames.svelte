7844a60b5eb18b1689dd5e5cf564b52cb401fc3a
Lucas-Tino <lucas.rosa131@etec.sp.gov.br>
2024-06-25 11:13:50 -0300

    feat: added user page

---
+++ src/components/user_page/FavoriteGames.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let gamename="Multiversus", gameimage = "", gamelink = "";
+</script>
+
+<div>
+    <a href="{gamelink}" class="flex flex-row bg-color-mid w-full rounded-3xl overflow-hidden hover:-translate-y-2 duration-75">
+        <img class="w-20 aspect-square" src="{gameimage}">
+        <p class="my-auto mx-5 md:text-xl text-lg text-color-body break-words">{gamename}</p>
+    </a>
+</div>
+
+<style>
+
+</style>
--- src/routes/UserPage.svelte
+++ src/routes/UserPage.svelte
@@ -1,11 +1,113 @@
 <script>
+    import Footer from "../components/Footer.svelte";
+    import Header from "../components/Header.svelte";
+    import FavoriteGames from "../components/user_page/FavoriteGames.svelte";
+
     export let params = {};
     let isUser = params.id.split("i")[0];
     let userID = params.id.split("i")[1];
 
+    var editingname = false, editingbio = false, biolength;
+
+    export var name = "User Name", bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.";
+
+
+    function editName() {
+        var usernamediv = document.getElementById('usernamediv');
+        var usernametext = document.getElementById('usernametext');
+        var usernameinput = document.getElementById('usernameinput');
+        var usernameediticon = document.getElementById('usernameediticon');
+
+        editingname = !editingname;
+
+        if (editingname == true) {
+            usernamediv.classList.add('bg-color-mid', 'border-b-2', 'border-l-2', 'border-[#69D3C9]')
+            usernameinput.classList.remove('hidden');
+            usernametext.classList.add('hidden');
+            usernameediticon.src = 'public/static/media/img/check_24dp_FILL0_wght400_GRAD0_opsz24.svg';
+            usernameinput.focus();
+        } else {
+            usernamediv.classList.remove('bg-color-mid', 'border-b-2', 'border-l-2', 'border-[#69D3C9]')
+            usernameinput.classList.add('hidden');
+            usernametext.classList.remove('hidden');
+            usernameediticon.src = 'public/static/media/img/edit_24dp_FILL0_wght400_GRAD0_opsz24.svg';
+        }
+
+        name = usernameinput.value;
+    }
+
+    function editBio() {
+        var biotext = document.getElementById('biotext');
+        var bioinput = document.getElementById('bioinput');
+        var bioediticon = document.getElementById('bioediticon');
+        var biocharcount = document.getElementById('biocharcount');
+
+        editingbio = !editingbio;
+
+        if (editingbio == true) {
+            bioinput.classList.remove('hidden');
+            biocharcount.classList.remove('hidden');
+            biotext.classList.add('hidden');
+            bioediticon.src = 'public/static/media/img/check_24dp_FILL0_wght400_GRAD0_opsz24.svg';
+            bioinput.focus();
+        } else {
+            bioinput.classList.add('hidden');
+            biocharcount.classList.add('hidden');
+            biotext.classList.remove('hidden');
+            bioediticon.src = 'public/static/media/img/edit_24dp_FILL0_wght400_GRAD0_opsz24.svg';
+        }
+
+        biolength = bioinput.value.length;
+        bio = bioinput.value;
+    }
+
+    function characterCount() {
+        biolength = bioinput.value.length;
+    }
+
 </script>
 
-<div>
-    <p>{isUser}</p>
-    <p>{userID}</p>
+<div class="bg-color-0">
+    <!-- <p>{isUser}</p><p>{userID}</p> -->
+    <Header />
+    <div class="flex md:flex-row flex-col md:mx-24 bg-color-0 md:h-full rounded-2xl overflow-hidden mx-5 mb-10 mt-5">
+        <div class="flex flex-col relative md:w-1/3 md:h-auto w-full space-y-7 pb-5">
+            <img class="relative rounded-full w-2/3 aspect-square mx-auto shrink mt-8 ring-8 ring-[#69D3C9]" src="src/assets/img/profile picture placeholder.png" alt="image">
+            
+            <div class="flex flex-row mx-auto space-x-3 rounded-lg" id="usernamediv">
+                <input type="text" class="text-[150%] text-color-body w-full outline-none ps-2 pb-1 bg-color-mid hidden" value="{name}" id="usernameinput">
+                <p class="text-2xl text-center text-color-body break-words" id="usernametext">{name}</p>
+                <button on:click={editName}>
+                    <img src="public/static/media/img/edit_24dp_FILL0_wght400_GRAD0_opsz24.svg" alt="edit name" class="h-8" id="usernameediticon">
+                </button>
+            </div>
+    
+        </div>
+    
+        <div class="md:w-2/3 w-full">
+            <div class="flex flex-row w-auto space-x-3">
+                <p class="text-2xl mt-8 ms-10 text-color-title">Bio:</p>
+                <button on:click={editBio}>
+                    <img src="public/static/media/img/edit_24dp_FILL0_wght400_GRAD0_opsz24.svg" alt="edit name" class="relative h-8 mt-8" id="bioediticon">
+                </button>
+            </div>
+            <div class="md:mx-10 mx-2 mt-2 bg-color-mid rounded-2xl border-l-4 border-b-4 border-[#69D3C9]">
+                <p class="p-5 text-justify text-color-body" id="biotext">{bio}</p>
+                <textarea class="outline-none w-full resize-none p-5 text-color-body bg-color-mid hidden" on:input={characterCount} rows="10" maxlength="1500" id="bioinput">{bio}</textarea>
+                <p class="relative text-right text-color-body pb-2 pe-3 hidden" id="biocharcount">{biolength}/1500</p>
+            </div>
+    
+            <div class="md:mx-10 mx-2 mt-10 md:mb-16 mb-8">
+                <p class="text-2xl text-color-title mb-4">Favorite Games</p>
+                <div class="lg:columns-2 columns-1 space-y-5">
+                    <FavoriteGames />
+                    <FavoriteGames />
+                    <FavoriteGames />
+                    <FavoriteGames />
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <Footer />
 </div>
