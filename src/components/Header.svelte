3e33de972c22794e2e52ef0901bdb960575fc0a4
Fellipe Leonardo <112525075+rakRandom@users.noreply.github.com>
2024-06-25 23:47:42 +0000

    feat: improve Header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,6 +4,9 @@
     let isFontBig = false;
     let userID = 100;
 
+    $: userDropdown = false;
+    $: menuDropdown = false;
+
     const toggleTheme = () => {
         document.documentElement.classList.toggle('dark');
     };
@@ -15,14 +18,25 @@
             document.documentElement.style.fontSize = "16px";
         isFontBig = !isFontBig;
     }
+
+    const toggleMenu = () => {
+        if (menuDropdown == true)
+            menuDropdown = false;
+        else if (menuDropdown == false)
+            menuDropdown = true;
+    }
+
+    setInterval(() => {
+        console.log(menuDropdown);
+    }, 1000)
 </script>
 
-<header class="z-50 bg-[#001219] w-full md:h-[80px] h-[110px] {className}">
+<header class="z-50 bg-[#001219] w-full h-fit {className}">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
     
-    <nav class="md:max-w-[1024px] h-full mx-auto py-2 w-full">
+    <nav class="md:max-w-[1024px] h-fit mx-auto py-2 w-full">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="/" class="flex items-center md:space-x-3 space-x-1 rtl:space-x-reverse">
+            <a href="/" class="flex items-center md:space-x-3 space-x-1 rtl:space-x-reverse lg:mr-6">
                 <img src="logoImage.svg" class="md:h-8 h-7" alt="Logo" />
                 <span class="self-center md:text-2xl text-xl font-semibold whitespace-nowrap text-[#69D3C9]">The Game Tips</span>
             </a>
@@ -34,15 +48,15 @@
                     <img src="static/media/img/text_increase_24dp_FILL0_wght400_GRAD0_opsz24.svg" alt="Font size" class="md:h-8 pe-3 h-4">
                 </button>
                 <button type="button"
-                    class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-[#69D3C9]"
-                    id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown"
-                    data-dropdown-placement="bottom">
+                    class="flex text-sm bg-gray-800 rounded-full md:me-0 ring-2"
+                    class:ring-[#69D3C9]={userDropdown}
+                    on:click={() => userDropdown = !userDropdown}>
                     <span class="sr-only">Open user menu</span>
                     <img class="w-8 h-8 rounded-full" src="static/media/img/profile picture placeholder.png" alt="user">
                 </button>
                 <!-- Dropdown menu -->
-                <div class="z-50 hidden my-4 text-base list-none bg-[#001219] divide-y divide-[#69D3C9] rounded-lg shadow"
-                    id="user-dropdown">
+                {#if userDropdown}
+                <div class="absolute top-0 translate-y-[64px] z-50 my-4 text-base list-none bg-[#001219] divide-y divide-[#69D3C9] rounded-lg shadow">
                     <div class="px-4 py-3">
                         <span class="block text-sm text-white">User Name</span>
                     </div>
@@ -62,9 +76,10 @@
                         </li>
                     </ul>
                 </div>
-                <button data-collapse-toggle="navbar-user" type="button"
-                    class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-[#69D3C9] rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-[#69D3C9]"
-                    aria-controls="navbar-user" aria-expanded="false">
+                {/if}
+                <button type="button" on:click={toggleMenu}
+                    class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-[#69D3C9] rounded-lg md:hidden outline-none active:ring-2 active:ring-[#69D3C9]"
+                    >
                     <span class="sr-only">Open main menu</span>
                     <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="#69D3C9"
                         viewBox="0 0 17 14">
@@ -73,29 +88,31 @@
                     </svg>
                 </button>
             </div>
-            <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
+            {#if window.screen.width >= 768 || menuDropdown}
+            <div class="items-center justify-between w-full md:flex md:w-auto md:order-1 max-md:mb-4" id="navbar-user">
                 <ul
-                    class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-[#69D3C9] rounded-lg bg-[#001219] AAAAAAAFDF] md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
+                    class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-[#69D3C9] max-md:bg-color-mid bg-transparent AAAAAAAFDF] md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0
+                           max-md:text-color-body text-white *:*:block *:*:py-2 *:*:px-3 *:*:rounded max-md:divide-y max-md:dark:divide-y-2 divide-black dark:divide-white">
                     <li>
                         <a href="/"
-                            class="block py-2 px-3 text-white t AAAAAAA rounded hover:bg-[#69D3C9] md:hover:bg-transparent md:hover:text-[#69D3C9] md:p-0">Home</a>
+                            class="hover:bg-[#69D3C9] hover:text-black md:hover:bg-transparent md:hover:text-[#69D3C9] md:p-0">Home</a>
                     </li>
                     <li>
                         <a href="/#/about"
-                            class="block py-2 px-3 text-white t AAAAAAA rounded hover:bg-[#69D3C9] md:hover:bg-transparent md:hover:text-[#69D3C9] md:p-0">About Us</a>
+                            class="hover:bg-[#69D3C9] hover:text-black md:hover:bg-transparent md:hover:text-[#69D3C9] md:p-0">About Us</a>
                     </li>
                     <li>
                         <a href="/#/help"
-                            class="block py-2 px-3 text-white t AAAAAAA rounded hover:bg-[#69D3C9] md:hover:bg-transparent md:hover:text-[#69D3C9] md:p-0">Help</a>
+                            class="hover:bg-[#69D3C9] hover:text-black md:hover:bg-transparent md:hover:text-[#69D3C9] md:p-0">Help</a>
                     </li>
                 </ul>
             </div>
+            {/if}
             
-            <div class="items-center flex flex-row w-full md:w-auto md:order-1 pt-1 md:pt-0">
-                <img src="static/media/img/search_24dp_FILL0_wght400_GRAD0_opsz24.svg" alt="Search" class="pe-2">
-                <div class="relative z-0">
-                    <input type="text" id="search_bar" class="block px-0 md:w-full text-white t AAAAAAA bg-transparent border-0 border-b-2 border-white AAAAAAAlack appearance-none focus:outline-none focus:ring-0 focus:border-[#69D3C9] peer" placeholder="" />
-                    <label for="search_bar" class="absolute md:block hidden text-white t AAAAAAA duration-300 transform -translate-y-7scale-75 top-0 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-[#69D3C9] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-7 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Search Tips...</label>
+            <div class="items-center flex lg:flex-1 lg:mx-[64px] flex-row-reverse gap-4 w-full md:w-auto md:order-1 pt-1 md:pt-0">
+                <img src="static/media/img/search_24dp_FILL0_wght400_GRAD0_opsz24.svg" alt="Search" class="me-2 p-2 rounded-lg bg-color-mid border-2 dark:border border-transparent dark:hover:border-white hover:border-[#1E9B8F] active:border-[#69D3C9] dark:active:border-[#69D3C9] cursor-pointer">
+                <div class="w-full relative z-0">
+                    <input type="text" id="search_bar" placeholder="Search..." class="block w-full text-color-body px-2 py-2 bg-color-mid border-0 border-b-2 border-[#1E9B8F] dark:border-white appearance-none focus:outline-none focus:ring-0 focus:border-[#69D3C9] peer" />
                 </div>
             </div>
         </div>
