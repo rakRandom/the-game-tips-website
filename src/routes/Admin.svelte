7c81694c830cafa31f9e231de3f00ba4e732c1d8
Fellipe Leonardo <112525075+rakRandom@users.noreply.github.com>
2024-06-25 12:42:34 +0000

    feat: improve Admin

--- src/routes/Admin.svelte
+++ src/routes/Admin.svelte
@@ -1,20 +1,37 @@
 <script>
   import { apiLink } from "../stores";
 
-    let confirmed = true;
+    let confirmed = false;
 
     function confirmCredentials() {}
 
     async function getContent() {
         let path = document.getElementById("url").value;
-        let content = await fetch(path).then(res => res.json());
+        let payload = await fetch($apiLink + "json" + path);
+        let content = await payload.json();
 
         if (!content)
-            return
+            return;
 
-        document.getElementById("content").innerText = content;
+        document.getElementById("content").innerHTML = JSON.stringify(content, null, 4);
     }
-    function putContent() {}
+
+    async function putContent() {
+        let path = document.getElementById("url").value;
+        let content = document.getElementById("content").value;
+
+        return await fetch($apiLink + "json" + path, {
+            method: 'POST',
+            mode: 'no-cors',
+            headers: {
+                'Content-type':'application/json', 
+                'Accept':'application/json'
+            },
+            body: JSON.stringify({
+                content: content
+            })
+        });
+    }
 </script>
 
 <div class="w-full h-svh bg-color-0">
@@ -44,7 +61,7 @@
         </div>
     </div>
     {:else}
-    <div class="mx-auto w-fit h-fit p-10">
+    <div class="mx-auto w-full max-w-[1280px] h-fit p-10">
         <h1 class="font-title-medium text-color-title text-4xl text-center">
             Admin
         </h1>
@@ -52,16 +69,18 @@
         <div class="flex flex-col gap-4 mt-6 text-lg" >
             <div>
                 <label for="url" class="block text-color-title">URL</label>
-                <input type="url" name="url" id="url" value={$apiLink} class="text-black w-[640px] h-full px-2 py-3 rounded-md outline-none">
-                <div class="inline *:p-2 *:border *:border-[#1E9B8F] dark:*:border-[#69D3C9] *:rounded-lg hover:*:bg-[#69D3C9] hover:*:text-black">
-                    <button type="button" on:click={getContent}>GET</button>
-                    <button type="submit" on:click={putContent}>PUT</button>
+                <div class="flex gap-4 w-full">
+                    <input type="url" name="url" id="url" value="/" class="flex-1 text-black w-[800px] h-full px-2 py-3 rounded-md outline-none">
+                    <div class="inline *:py-2 *:px-4 *:border *:border-[#1E9B8F] dark:*:border-[#69D3C9] *:rounded-lg hover:*:bg-[#69D3C9] hover:*:text-black">
+                        <button type="button" on:click={getContent}>GET</button>
+                        <button type="submit" on:click={putContent}>PUT</button>
+                    </div>
                 </div>
             </div>
 
             <div>
                 <label for="json" class="block text-color-title">Content</label>
-                <textarea name="json" id="content" class="text-base text-black w-full max-w-[1024px] min-h-[500px] p-2 rounded-md outline-none overflow-y-auto"></textarea>
+                <textarea name="json" id="content" class="text-base text-black w-full max-w-[1280px] min-h-[500px] p-2 rounded-md outline-none overflow-y-auto"></textarea>
             </div>
         </div>
     </div>
