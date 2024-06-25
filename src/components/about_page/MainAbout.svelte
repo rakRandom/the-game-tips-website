82ed23424180f13b47fad6790a0823c1bebcd198
Fellipe Leonardo <112525075+rakRandom@users.noreply.github.com>
2024-06-25 11:29:23 +0000

    fix: MainAbout image

--- src/components/about_page/MainAbout.svelte
+++ src/components/about_page/MainAbout.svelte
@@ -2,9 +2,9 @@
     let aboutText = "The Game Tips is a website that focuses on sharing tips with gamers, helping them get through hard levels and challenges, or be better at online games. The idea for this project came from our team’s own experiences with gaming, as well as our passion for games. We started out in 2024, and have been providing information for gamers since then."
 </script>
 
-<div class="w-full max-w-[1280px] h-fit mx-auto lg:bg-[#001B28]">
+<div class="w-full max-w-[1280px] lg:min-w-[1024px] h-fit lg:min-h-[500px] mx-auto lg:bg-[#001B28] xl:rounded-b-xl overflow-hidden">
     <div class="relative h-full w-full">
-        <img src="backgroundImageAbout.svg" alt="" class="hidden lg:block object-cover xl:rounded-b-lg">
+        <img src="backgroundImageAbout.svg" alt="" class="hidden lg:block object-cover">
         
         <div class="lg:absolute top-0 w-full h-full">
             <div class="z-10 flex items-center justify-center w-full h-full lg:py-24">
