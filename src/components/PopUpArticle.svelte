df33e68b73d79908c1c255293612bd208e29f4a8
Fellipe Leonardo <112525075+rakRandom@users.noreply.github.com>
2024-06-26 02:19:41 +0000

    fix: PopUpArticle redirect clickable area

--- src/components/PopUpArticle.svelte
+++ src/components/PopUpArticle.svelte
@@ -14,20 +14,20 @@
 <div class="h-[280px] min-w-[220px] max-w-[220px] {className}">
     <div class="relative w-full h-[75%]">
         <img src={fetchImage(imgSrc)} alt="" class="w-full h-full object-cover rounded-lg">
-        <div class="absolute w-full h-[53%] -bottom-16">
+        <a href={"#/article/" + articlePage} class="absolute w-full h-[53%] -bottom-16">
             <div class="flex w-full h-full justify-center pt-1 hover:-translate-y-1 transition-transform">
                 <div class="flex flex-col text-center w-[90%] h-full bg-color-mid rounded-md overflow-hidden cursor-pointer">
                     <div class="flex-1 py-1 px-1">
                         <h3 class="font-title-medium text-color-title uppercase pb-1"> {gameName} </h3>
                         <p class="text-sm line-clamp-2 text-color-body"> {title} </p>
                     </div>
-                    <a href={"#/article/" + articlePage} class="flex items-center gap-2 text-[14px] font-light p-1 bg-[#051D26]">
+                    <a href="/" class="flex items-center gap-2 text-[14px] font-light p-1 bg-[#051D26]">
                         <img src={authorPhotoSrc} alt="" class="inline-block h-6 aspect-square border-[1px] border-[#69D3C9BE] rounded-full">
                         <span class="text-[#69D3C9BE] w-full text-center">By {authorName} - {postDate}</span>
                     </a>
                 </div>
             </div>
-        </div>
+        </a>
     </div>
 </div>
 
